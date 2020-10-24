4af9b632a869117b596060c70fc7401ee27fbf30
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-10-24 17:04:47 -0400

    finished extraction technique

---
+++ extract_transform_load.py
@@ -0,0 +1,33 @@
+"""
+Extract, transform, and load the data
+run this script in a folder with an 'input/' folder, which contains the .mat files that you want to transform
+"""
+
+from scipy.io import loadmat
+
+def extractor(path) :
+    """
+
+    :param path: path to the .mat file that you wish to extract
+    :return: numpy array of the wave data. Data is expressed in integer form
+    """
+    data = loadmat(path)
+    filename = path.split('\\')[-1]
+    if not filename.startswith('Patient') or not filename.startswith('Dog') :
+        filename = path.split('/')[-1]
+
+    assert 'interictal' in filename or 'preictal' in filename, f'File must be interictal or preictal. ' \
+                                                               f'You probably passed a test file. Your file: {path}'
+
+    y = 1 if 'interictal' in filename else 0
+
+    for k,v in data.items() :
+        if k.startswith('interictal') or k.startswith('preictal') :
+            return v[0][0][0]
+
+    return None
+
+if __name__ == '__main__' :
+    data = extractor('./input/Patient_1/Patient_1/Patient_1_preictal_segment_0001.mat')
+
+    print(data)
--- train.py
+++ train.py
@@ -16,9 +16,86 @@ def seed_everything(seed=0) :
     except :
         pass
     return
-seed_everything(42)
+seed_everything(seed:=42)
+
+
+
+def get_paths(seed=42) :
+    """
+
+    :return: lists of interictal and preictal paths to put in a dataset
+    """
+
+    types = ['interictal_segment', 'preictal_segment']
+    interictal_paths = []
+    preictal_paths = []
+
+    for root, dirs, files in os.walk('./input'):
+        for i, file in enumerate(files):
+            path = os.path.join(root, file)
+            segment = path[:-9]
+            if segment.endswith(types[0]):
+                interictal_paths.append(path)
+                continue
+            if segment.endswith(types[1]):
+                preictal_paths.append(path)
+            # there are test file types with no answer for a kaggle competition, so skip them
+
+    # shuffle paths
+    random.Random(seed).shuffle(interictal_paths)
+    random.Random(seed).shuffle(preictal_paths)
+
+    return interictal_paths,preictal_paths
+
+interictal_paths,preictal_paths = get_paths(seed)
+
+assert interictal_paths == get_paths(seed)[0]
+assert preictal_paths == get_paths(seed)[1]
+
+
+def load_from_paths(interictal_paths,preictal_paths,type_size=None) :
+    """
+
+    :param interictal_paths:
+    :param preictal_paths:
+    :param type_size:
+    :return:
+    """
+
+    if type_size is None :
+        type_size = len(interictal_paths)
+
+    # for each file, preprocess the file and add to dataset
+    assert type_size <= len(interictal_paths), f'Max type_size: {len(interictal_paths)}'
+    len_preictal_paths = len(preictal_paths)
+
+    for i in range(type_size) :
+        path = interictal_paths[i]
+        data = scipy.io.loadmat(path)
+        filename = path.split('/')[-1]
+        k = filename.split('_')[2]
+
+
+        if i >= len_preictal_paths : continue
+
+    return
+
+load_from_paths(interictal_paths=interictal_paths,preictal_paths=preictal_paths,type_size=20)
+
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.utils import to_categorical
 
+batch_size = 128
+num_classes = 2
+epochs = 100
+img_rows, img_cols = 256,8
+
+def build_model() :
+    model = Sequential([
+        Conv2D(16, kernel_size=(3, 3), activation='relu',input_shape=None)
+    ])
+
+    return model
