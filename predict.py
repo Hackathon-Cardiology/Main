0608498022b1778455a21bf900ff1b5457223889
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-10-25 09:45:23 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,25 +6,26 @@ import sys
 sys.path.append("./")
 from etl import ETL
 
-X,y = ETL('./input','Patient_2').extract_transform_load()
+X,y = ETL('./input',['Patient_1_preictal_segment_0005.mat']).extract_transform_load()
 model = tf.keras.models.load_model('./model')
-
+print(X.shape)
 predictions = np.array(model.predict(X)).astype(np.float64)
 
 from sklearn.metrics import log_loss,accuracy_score,roc_auc_score
 
-print('log loss')
-print(log_loss(y_true=y,y_pred=predictions))
+print('\nlog loss:')
+print(log_loss(y_true=y,y_pred=predictions,labels=[0,1]))
 
-
-print('roc')
-print(roc_auc_score(y_true=y, y_score=predictions))
-
-
-print('accuracy')
+try :
+    print('\nroc:')
+    print(roc_auc_score(y_true=y, y_score=predictions,labels=[0,1]))
+except ValueError :
+    print(np.nan)
+print('\naccuracy:')
+acc_preds = predictions.copy()
 for i in range(len(predictions)) :
     if predictions[i] > .5 :
-        predictions[i] = 1
-    else : predictions[i] = 0
-print(accuracy_score(y_true=y,y_pred=predictions))
+        acc_preds[i] = 1
+    else : acc_preds[i] = 0
+print(accuracy_score(y_true=y,y_pred=acc_preds))
 
